fae6a8fe1be9eab99672be5c8e0c0684e7883bc0
Nuriddin Obidjonov <nuriddin.obidjonov777@gmail.com>
2023-02-28 10:47:09 +0500

    completed lesson 55

--- util/config.go
+++ util/config.go
@@ -13,6 +13,7 @@ type Config struct {
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
+	RedisAddress         string        `mapstructure:"REDIS_ADDERSS"`
 	HttpServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GrpcServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
