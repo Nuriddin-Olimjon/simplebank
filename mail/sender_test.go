efbe0560fea8aabdef60d7592070a8bb22981941
Nuriddin Obidjonov <nuriddin.obidjonov777@gmail.com>
2023-02-28 15:15:37 +0500

    completed lesson 59

---
+++ mail/sender_test.go
@@ -0,0 +1,27 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/Nuriddin-Olimjon/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email from Simple Bank"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="https://google.com">Simple Bank Service</a></p>
+	`
+
+	to := []string{"nuriddin.obidjonov7777@gmail.com", "nuriddin_obidjonov@mail.ru"}
+	attachFiles := []string{"../start.sh"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
